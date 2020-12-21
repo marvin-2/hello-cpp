d1dffbe12d6ebdea4820508f4a4c90d04deb8188
Marvin <64291623+marvin-2@users.noreply.github.com>
2020-12-21 16:31:48 -0500

    Init commit

---
+++ helloworld.cpp
@@ -0,0 +1,21 @@
+// Hello world program
+// https://code.visualstudio.com/docs/cpp/config-mingw
+
+// Setup auto build: Terminal -> Configure Default Build Task -> g++
+// Build: Ctrl+Shift+B
+
+// Setup debugger: Run -> Add Configuration -> C++ (GBD/LLDB) -> g++
+// Build & Debug: F5
+
+// Run from command line: ./helloworld.exe
+
+#include <iostream>
+
+int main() {
+
+  int a = 1;
+
+  std::cout << "Hello world!";
+  return 0;
+
+}
