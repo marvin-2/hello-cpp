b88a31c892f25d597a6648002a4e47343644a676
Marvin <64291623+marvin-2@users.noreply.github.com>
2021-01-02 16:21:06 -0500

    Multiple source files

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,6 +3,7 @@
 
 // Windows 10 
 // Setup auto build: Terminal -> Configure Default Build Task -> g++
+// Building multiple CPP files, change tasks.json args: from "${file}" to "${fileDirname}/*.cpp"
 // Build: Ctrl+Shift+B
 
 // Setup debugger: Run -> Add Configuration -> C++ (GBD/LLDB) -> g++
@@ -11,12 +12,15 @@
 // Run from command line: ./helloworld.exe
 
 #include <iostream>
+#include <string>
 
-int main() {
+using namespace std;
 
-  int a = 1;
+int hello() {
 
-  std::cout << "Hello world!";
+  string msg = "Hello world!";
+
+  cout << msg;
   return 0;
 
 }
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "helloworld.h"
+
+using namespace std;
+
+int main() {
+  hello();
+  return 0;
+}
